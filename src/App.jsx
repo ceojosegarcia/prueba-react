d6500c0c80599f450a83071c52b02a1e76ad40bf
Josegarcia <Josegarcia@vidalradio.ejemplo.com>
2023-08-08 20:21:53 -0500

    animacion con dialogo linea 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { useState , useRef , useEffect} from 'react'
 
 function App() {
 
-  const dialogoId = [60,8,3,6,5,11,67,50,37,60,52,8,12,11,5,12,37,55,25,22,60,45,12,52,10,65,35]
+  //const dialogoId = [60,8,3,6,5,11,67,50,37,60,52,8,12,11,5,12,37,55,25,22,60,45,12,52,10,65,35]
+  const dialogoId = [6,3,6,5,6,10,4,6,5,14,10,4,8,5,3,14,6,10,12,10,6,2]
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [isPlayDisabled, setPlayDisabled] = useState(false)
@@ -17,20 +18,21 @@ function App() {
     let contIndex = 0;
     let posY = "0%"
     let posX = "0%"
-    let indexDialogo = 0;
+    let indexDialogo = 0
+    let indexActual = 0
 
     if (isRunning) {
       interval = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % dialogoId.length)
-        
-        posY= `${expresiones[indexDialogo].y * (-100)}%`
-        posX= `${expresiones[indexDialogo].x * (-100)}%`
+        indexActual = dialogoId[contIndex]
+        posY= `${expresiones[indexActual].y * (-100)}%`
+        posX= `${expresiones[indexActual].x * (-100)}%`
         setRostroY((newY) => posY)
         setRostroX((newX) => posX)
 
         contIndex++
         
-        console.log(`top: ${posY} left: ${posX}`)
+        console.log(`index: ${indexActual} top: ${posY} left: ${posX}`)
         if(contIndex > (dialogoId.length-1)){
           posY = "0%"
           posX = "0%"
@@ -40,7 +42,7 @@ function App() {
         }else{
           indexDialogo = dialogoId[contIndex]
         }
-      }, 300);
+      }, 250);
     } else {
       clearInterval(interval);
     }
